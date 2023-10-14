b93e6e1c82cf00492f45facd79d91e0882953a81
juntab1 <jsbwhaleguy@gmail.com>
2023-10-14 15:58:16 -0700

    set up for Ai functions

---
+++ Ai.py
@@ -0,0 +1,17 @@
+
+
+class AiPictureWord:
+    
+    def __init__(self):
+        pass
+
+    def turn_image_word(self):
+        pass
+
+
+def main():
+    Ai = AiPictureWord()
+    Ai.turn_image_word()
+
+if __name__ == "__main__":
+    main()
