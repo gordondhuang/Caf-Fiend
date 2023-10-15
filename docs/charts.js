781ebba0353a61ff393b735973e28f9cadf83623
Gordon Huang <gdhua@cs.washington.edu>
2023-10-15 07:02:32 -0700

    water and caffeine chart functional

--- docs/charts.js
+++ docs/charts.js
@@ -1,5 +1,6 @@
 // Get the canvas element where the chart will be rendered
 const ctx = document.getElementById('myChart').getContext('2d');
+const wtx = document.getElementById('waterChart').getContext('2d');
 
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
 
@@ -11,12 +12,23 @@ const weekday = daysOfWeek[currentDay];
 document.getElementById("currentWeekDay").innerHTML = weekday;
 
 // Define your chart data and options
-const chartData = {
+const caffeineData = {
     labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'],
     datasets: [{
         label: 'Caffeine',
         data: [400, 400, 400, 400, 400, 400, 400], // Example data
         backgroundColor: 'rgba(108,88,76,1)', // Example background color
+        borderColor: 'rgba(75, 192, 192, 1)', // Example border color
+        borderWidth: 1, // Example border width
+    }]
+};
+
+const waterData = {
+    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'],
+    datasets: [{
+        label: 'Water',
+        data: [0, 0, 0, 0, 0, 0, 0], // Example data
+        backgroundColor: 'rgba(103,165,169,1)', // Example background color
         borderColor: 'rgba(75, 192, 192, 1)', // Example border color
         borderWidth: 1, // Example border width
     }]
@@ -33,7 +45,14 @@ const chartOptions = {
 // Create a new chart instance
 const myChart = new Chart(ctx, {
     type: 'bar', // Specify the chart type (e.g., 'bar', 'line', 'pie', etc.)
-    data: chartData,
+    data: caffeineData,
+    options: chartOptions
+});
+
+// Create a new chart instance
+const waterChart = new Chart(wtx, {
+    type: 'bar', // Specify the chart type (e.g., 'bar', 'line', 'pie', etc.)
+    data: waterData,
     options: chartOptions
 });
 
